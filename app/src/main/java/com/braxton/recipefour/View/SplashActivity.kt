6f72b5a92f8f31599ae7d7a339aff15076c3f795
KimikoXmaka <113034039+KimikoXmaka@users.noreply.github.com>
2023-01-18 10:22:16 -0600

    Splash

--- app/src/main/java/com/braxton/recipefour/View/SplashActivity.kt
+++ app/src/main/java/com/braxton/recipefour/View/SplashActivity.kt
@@ -1,11 +1,12 @@
-package com.braxton.recipefour
+package com.braxton.recipefour.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.braxton.recipefour.R
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
     }
 }
