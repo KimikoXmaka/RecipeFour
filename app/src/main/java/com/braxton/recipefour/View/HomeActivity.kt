0168d68207efceb3012db72a19a5f5c4649d0af0
KimikoXmaka <113034039+KimikoXmaka@users.noreply.github.com>
2023-01-19 09:55:44 -0600

    Recycle

---
+++ app/src/main/java/com/braxton/recipefour/View/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.braxton.recipefour.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.braxton.recipefour.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
