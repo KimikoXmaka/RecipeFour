73f8304fc47a346a950b1bc6917d20d5215696a2
KimikoXmaka <113034039+KimikoXmaka@users.noreply.github.com>
2023-04-05 09:18:06 -0500

    Ihatethis

--- app/src/main/java/com/braxton/recipefour/View/SplashActivity.kt
+++ app/src/main/java/com/braxton/recipefour/View/SplashActivity.kt
@@ -3,10 +3,22 @@ package com.braxton.recipefour.View
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.braxton.recipefour.Model.Category
+import com.braxton.recipefour.Model.GetDataService
+import com.braxton.recipefour.Model.RetroClientInstance
 import com.braxton.recipefour.databinding.ActivitySplashBinding
+import pub.devrel.easypermissions.AppSettingsDialog
+import pub.devrel.easypermissions.EasyPermissions
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BaseActivity(), EasyPermissions.RationaleCallbacks, EasyPermissions.PermissionCallbacks {
+
+    private var READ_STORAGE_PERM = 123
 
     private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,10 +26,80 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        readStorageTask()
+
         binding.btnGetStarted.setOnClickListener {
             var intent = Intent(this@SplashActivity, HomeActivity::class.java)
             startActivity(intent)
             finish()
+        }
+    }
+
+    fun getCategories() {
+        val service = RetroClientInstance.retrofitInstance!!.create(GetDataService::class.java)
+        val call = service.getCategoryList()
+        call.enqueue(object: Callback<Category> {
+            override fun onResponse(call: Call<Category>, response: Response<Category>) {
+
+                insertDataIntoRoomDb(response.body())
+
+            }
+
+            override fun onFailure(call: Call<Category>, t: Throwable) {
+               // loader.visiblity = View.INVISIBLE
+                Toast.makeText(this@SplashActivity, "Your a loser", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+    }
+
+    fun insertDataIntoRoomDb(category: Category?) {
+
+    }
+
+    private fun hasReadStoragePermission(): Boolean {
+        return EasyPermissions.hasPermissions(
+            this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+    }
+
+    private fun readStorageTask() {
+        if (hasReadStoragePermission()) {
+            getCategories()
+        } else {
+            EasyPermissions.requestPermissions(
+                this,
+                "This app needs access to your storage.",
+                READ_STORAGE_PERM,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions,grantResults, this)
+    }
+    override fun onRationaleAccepted(requestCode: Int) {
+
+    }
+
+    override fun onRationaleDenied(requestCode: Int) {
+
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
         }
     }
 }
--- app/src/main/java/com/braxton/recipefour/ViewModel/RecipeDao.kt
+++ app/src/main/java/com/braxton/recipefour/ViewModel/RecipeDao.kt
@@ -4,14 +4,19 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.braxton.recipefour.Model.Category
+import com.braxton.recipefour.Model.CategoryItems
 
 
 @Dao
 interface RecipeDao {
 
-    @Query("SELECT * FROM recipes ORDER BY id DESC")
-    suspend fun getAllCategory(): List<Recipes>
+    @Query("SELECT * FROM category ORDER BY id DESC")
+    suspend fun getAllCategory(): List<Category>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertCategory(categoryItems: Recipes)
+    suspend fun insertCategory(categoryItems: CategoryItems)
+
+    @Query("DELETE FROM categoryitems")
+    suspend fun clearDb()
 }
